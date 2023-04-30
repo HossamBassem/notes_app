e07e8558ce7543825574578a75aad4972ae90ca3
Hossam bassem <hossambassem33@gmail.com>
2023-04-30 12:02:52 +0300

    add note item

---
+++ lib/views/widgets/custom_note_icon.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(bottom: 24, top: 24, left: 16),
+      decoration: BoxDecoration(
+        color: Colors.yellow,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+        ListTile(
+          title: Text(
+            'Flutter tips',
+            style: TextStyle(color: Colors.black, fontSize: 26),
+          ),
+          subtitle: Padding(
+            padding: const EdgeInsets.only(top: 16, bottom: 16),
+            child: Text(
+              "Build your career with tharwat samy",
+              style:
+                  TextStyle(color: Colors.black.withOpacity(0.5), fontSize: 18),
+            ),
+          ),
+          trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                FontAwesomeIcons.trash,
+                color: Colors.black,
+                size: 24,
+              )),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(right: 24),
+          child: Text(
+            '30 Apr 2023',
+            style:
+                TextStyle(color: Colors.black.withOpacity(0.5), fontSize: 16),
+          ),
+        ),
+      ]),
+    );
+  }
+}
---
+++ lib/views/widgets/notes_view_body.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/views/widgets/custom_appbar.dart';
+
+import 'custom_note_icon.dart';
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: const [
+          SizedBox(
+            height: 50,
+          ),
+          CustomAppBar(),
+          NoteItem(),
+        ],
+      ),
+    );
+  }
+}
