4d9bf55f670746c763e0d1628a438e70ac62bd30
Hossam bassem <hossambassem33@gmail.com>
2023-05-01 14:21:37 +0300

    create edit note view

--- lib/views/widgets/custom_note_icon.dart
+++ lib/views/widgets/custom_note_icon.dart
@@ -1,49 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(bottom: 24, top: 24, left: 16),
-      decoration: BoxDecoration(
-        color: Colors.yellow,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
-        ListTile(
-          title: Text(
-            'Flutter tips',
-            style: TextStyle(color: Colors.black, fontSize: 26),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(
+          builder: (context) {
+            return const EditNoteView();
+          },
+        ));
+      },
+      child: Container(
+        padding: EdgeInsets.only(bottom: 24, top: 24, left: 16),
+        decoration: BoxDecoration(
+          color: Colors.yellow,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+          ListTile(
+            title: Text(
+              'Flutter tips',
+              style: TextStyle(color: Colors.black, fontSize: 26),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(top: 16, bottom: 16),
+              child: Text(
+                "Build your career with tharwat samy",
+                style: TextStyle(
+                    color: Colors.black.withOpacity(0.5), fontSize: 18),
+              ),
+            ),
+            trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  FontAwesomeIcons.trash,
+                  color: Colors.black,
+                  size: 24,
+                )),
           ),
-          subtitle: Padding(
-            padding: const EdgeInsets.only(top: 16, bottom: 16),
+          Padding(
+            padding: const EdgeInsets.only(right: 24),
             child: Text(
-              "Build your career with tharwat samy",
+              '30 Apr 2023',
               style:
-                  TextStyle(color: Colors.black.withOpacity(0.5), fontSize: 18),
+                  TextStyle(color: Colors.black.withOpacity(0.5), fontSize: 16),
             ),
           ),
-          trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(
-                FontAwesomeIcons.trash,
-                color: Colors.black,
-                size: 24,
-              )),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(right: 24),
-          child: Text(
-            '30 Apr 2023',
-            style:
-                TextStyle(color: Colors.black.withOpacity(0.5), fontSize: 16),
-          ),
-        ),
-      ]),
+        ]),
+      ),
     );
   }
 }
