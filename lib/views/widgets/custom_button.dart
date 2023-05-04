35b63000c0efe987514c9a2b4f23aae5412319b5
Hossam bassem <hossambassem33@gmail.com>
2023-05-04 14:37:19 +0300

    create fetch note method

--- lib/views/widgets/custom_button.dart
+++ lib/views/widgets/custom_button.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/constants.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key, this.onTap});
+  const CustomButton({super.key, this.onTap, this.isLoading = false});
   final void Function()? onTap;
+  final bool isLoading;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -14,11 +15,20 @@ class CustomButton extends StatelessWidget {
         decoration: BoxDecoration(
             color: kPraimaryColor, borderRadius: BorderRadius.circular(16)),
         child: Center(
-            child: Text(
-          'Add',
-          style: TextStyle(
-              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
-        )),
+            child: isLoading
+                ? const SizedBox(
+                    height: 24,
+                    width: 24,
+                    child: CircularProgressIndicator(
+                      color: Colors.black,
+                    ))
+                : const Text(
+                    'Add',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  )),
       ),
     );
   }
